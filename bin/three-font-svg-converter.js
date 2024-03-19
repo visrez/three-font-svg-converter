9755d8840665a1f45af97842498eff94e570dfbd
Hugo Sales <hugo@visrez.com>
2024-03-19 03:15:18 +0000

    Extend parser to handle path actions needed to convert FontAwesome's copy icon

--- bin/three-font-svg-converter.js
+++ bin/three-font-svg-converter.js
@@ -97,11 +97,19 @@ function svgToThree(data) {
   const maxX = width + minX;
   const maxY = height + minY;
 
-  const pathData = svgDocument.documentElement.querySelector('svg path').attributes.getNamedItem('d').value;
+  const pathData = svgDocument.documentElement.querySelector('svg path').attributes.getNamedItem('d').value
+    .replaceAll(/([a-zA-Z])([a-zA-Z])/g, '$1 $2') // add spaces between consecutive actions
+    .replaceAll(/([^ ])?([a-zA-Z])([^ ])?/g, '$1 $2 $3') // add spaces around actions not followed by spaces
+    .replaceAll(/([^ ])-/g, '$1 -') // add space before minus sign
+    .replaceAll(/,/g, '') // remove all commas, not strictly necessary, so they're sometimes omitted
+    .replaceAll(/ +/g, ' ') // collapse double spaces
+    .trim();
 
   const input = pathData.split(' ');
   let path = '';
 
+  let x = 0;
+  let y = 0;
   for (
     let i = 0;
     i < input.length;
@@ -109,44 +117,101 @@ function svgToThree(data) {
     const action = input[i++];
     if (action === '') break;
     switch (action) {
-      case 'M': { // move
-        const x = input[i++];
-        const y = input[i++];
+      case 'm': { // move (relative)
+        x = +input[i++] + x;
+        y = +input[i++] + y;
 
         path += `m ${x} ${y} `;
         break;
       }
-      case 'L': { // lineTo
-        const x = input[i++];
-        const y = input[i++];
+      case 'M': { // move
+        x = +input[i++];
+        y = +input[i++];
+
+        path += `m ${x} ${y} `;
+        break;
+      }
+      case 'l': { // line (relative)
+        x = +input[i++] + x;
+        y = +input[i++] + y;
 
         path += `l ${x} ${y} `;
         break;
       }
-      case 'Q': { // quadraticCurveTo
-        const controlPointX1 = input[i++];
-        const controlPointY1 = input[i++].slice(0, input[i - 1].length - 1);
-        const x = input[i++];
-        const y = input[i++];
+      case 'L': { // line
+        x = +input[i++];
+        y = +input[i++];
 
-        // path.quadraticCurveTo(controlPointX1, controlPointY1, x, y);
+        path += `l ${x} ${y} `;
+        break;
+      }
+      case 'h': { // horizontal line (relative)
+        x = +input[i++] + x;
+
+        path += `l ${x} ${y} `;
+        break;
+      }
+      case 'H': { // horizontal line
+        x = +input[i++];
+
+        path += `l ${x} ${y} `;
+        break;
+      }
+      case 'v': { // vertical line (relative)
+        y = +input[i++] + y;
+
+        path += `l ${x} ${y} `;
+        break;
+      }
+      case 'V': { // vertical line
+        y = +input[i++];
+
+        path += `l ${x} ${y} `;
+        break;
+      }
+      case 'q': { // quadratic bezier (relative)
+        const controlPointX1 = +input[i++] + x;
+        const controlPointY1 = +input[i++] + y;
+        x = +input[i++] + x;
+        y = +input[i++] + y;
+
         path += `q ${x} ${y} ${controlPointX1} ${controlPointY1} `;
         break;
       }
-      case 'C': { // bezierCurveTo
+      case 'Q': { // quadratic bezier
         const controlPointX1 = input[i++];
-        const controlPointY1 = input[i++].slice(0, input[i - 1].length - 1);
-        const controlPointX2 = input[i++];
-        const controlPointY2 = input[i++].slice(0, input[i - 1].length - 1);
-        const x = input[i++];
-        const y = input[i++];
+        const controlPointY1 = input[i++];
+        x = +input[i++];
+        y = +input[i++];
 
-        // path.bezierCurveTo(controlPointX1, controlPointY1, controlPointX2, controlPointY2, x, y);
+        path += `q ${x} ${y} ${controlPointX1} ${controlPointY1} `;
+        break;
+      }
+      case 'c': { // cubic bezier (relative)
+        const controlPointX1 = +input[i++] + x;
+        const controlPointY1 = +input[i++] + y;
+        const controlPointX2 = +input[i++] + x;
+        const controlPointY2 = +input[i++] + y;
+        x = +input[i++] + x;
+        y = +input[i++] + y;
+
         path += `b ${x} ${y} ${controlPointX1} ${controlPointY1} ${controlPointX2} ${controlPointY2} `;
         break;
       }
-      case 'Z': {
-        path += 'z';
+      case 'C': { // cubic bezier
+        const controlPointX1 = input[i++];
+        const controlPointY1 = input[i++];
+        const controlPointX2 = input[i++];
+        const controlPointY2 = input[i++];
+        x = +input[i++];
+        y = +input[i++];
+
+        path += `b ${x} ${y} ${controlPointX1} ${controlPointY1} ${controlPointX2} ${controlPointY2} `;
+        break;
+      }
+      case 'z': // close path (relative)
+      case 'Z': { // close path
+        path += 'z ';
         break;
       }
       default: {
@@ -160,6 +225,8 @@ function svgToThree(data) {
     ha: 0, // horizontal advance? doesn't seem to be used
     x_min: minX,
     x_max: maxX,
+    y_min: minY,
+    y_max: maxY,
     o: path,
   };
   console.log(`\n\n${JSON.stringify(glyph)}`);
